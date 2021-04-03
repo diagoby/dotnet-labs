a39c8ee912c7a6cb1197a1bdb0f00da92c25eade
Vladislav Hutych <mandarineye@gmail.com>
2021-04-03 21:03:22 +0300

    Add ignored files

---
+++ Lab2/frmMain.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace Lab2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dtgridvMatrix = new System.Windows.Forms.DataGridView();
+            this.bindsrcMatrix = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgridvMatrix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindsrcMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgridvMatrix
+            // 
+            this.dtgridvMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgridvMatrix.ColumnHeadersVisible = false;
+            this.dtgridvMatrix.Location = new System.Drawing.Point(134, 76);
+            this.dtgridvMatrix.Name = "dtgridvMatrix";
+            this.dtgridvMatrix.RowTemplate.Height = 25;
+            this.dtgridvMatrix.Size = new System.Drawing.Size(330, 316);
+            this.dtgridvMatrix.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 471);
+            this.Controls.Add(this.dtgridvMatrix);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgridvMatrix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindsrcMatrix)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgridvMatrix;
+        private System.Windows.Forms.BindingSource bindsrcMatrix;
+    }
+}
+
---
+++ Lab2/frmMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            DataSet dataSet = new DataSet();
+            MatrixFileDataAdapter dataAdapter = new MatrixFileDataAdapter();
+            dataAdapter.Fill(dataSet);
+
+            DataView dataView = new DataView(dataSet.Tables[0]);
+            bindsrcMatrix.DataSource = dataView;
+            dtgridvMatrix.DataSource = bindsrcMatrix;
+        }
+    }
+}

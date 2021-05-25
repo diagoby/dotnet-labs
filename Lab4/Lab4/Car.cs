dd829ad991ba8467bc6fc8d4823ce61a11bb6ee9
Vladislav Hutych <mandarineye@gmail.com>
2021-05-25 23:44:19 +0300

    Implement Car POCO class for test with set

---
+++ Lab4/Lab4/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Car
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Car car &&
+                   Name == car.Name &&
+                   Year == car.Year &&
+                   Color == car.Color;
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {Name} of {Year}";
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(HashCode.Combine(Name, Color), Year);
+        }
+    }
+}
--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -8,12 +8,12 @@ namespace Lab4
         static void Main(string[] args)
         {
             LinkedSet<int> set1 = new LinkedSet<int>(new[] { 2, 3, 4 });
-            LinkedSet<int> set2 = new LinkedSet<int>(new[] { 2, 3, 4 });
+            LinkedSet<int> set2 = LinkedSet<int>.Of(new[] { 2, 3, 4 });
 
             Console.WriteLine(set1.Equals(set2));
 
             //LinkedSet<int> set1 = new LinkedSet<int>(new[] { 2, 3, 4 });
-            //LinkedSet<int> clone = (LinkedSet<int>) set1.Clone();
+            //LinkedSet<int> clone = (LinkedSet<int>)set1.Clone();
 
             //Console.WriteLine(set1.Equals(clone));
 
@@ -32,6 +32,26 @@ namespace Lab4
 
             //Console.WriteLine($"{set1.GetHashCode()},\n{set2.GetHashCode()}");
             //Console.WriteLine(set1 - set2);
+
+            //Cars();
+        }
+
+        static void Cars()
+        {
+            Car audi = new Car() { Name = "Audi", Year = 2013, Color = "Grey" };
+            Car audi2 = new Car() { Name = "Audi", Year = 2013, Color = "Grey" };
+            Car honda = new Car() { Name = "Honda", Year = 2010, Color = "Red" };
+
+            LinkedSet<Car> cars = new LinkedSet<Car>(new Car[] { audi, honda, null });
+            LinkedSet<Car> cars2 = new LinkedSet<Car>(new Car[] { audi2, honda });
+
+            //LinkedSet<Car> cars = new LinkedSet<Car>(new Car[] { audi, honda });
+            //LinkedSet<Car> clone = (LinkedSet<Car>) cars.Clone();
+            //clone.Remove(audi);
+            //clone.Add(audi);
+
+            Console.WriteLine(cars + cars2);
+            Console.WriteLine(cars.Equals(cars2));
         }
     }
 }
